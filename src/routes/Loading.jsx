f6f4b012d72077b5fd1292cc544132fb1fbf98cb
Priyansh Verma <verma.priyansh444@gmail.com>
2025-07-29 17:33:12 +0530

    updated loader

--- src/routes/Loading.jsx
+++ src/routes/Loading.jsx
@@ -1,27 +1,42 @@
 import { FaSpinner } from "react-icons/fa";
-import { Text, Image } from "@chakra-ui/react";
-import logo from "../../src/assets/img/veda-bot-favicon.png";
+import { Text } from "@chakra-ui/react";
 import bgimg from "../../src/assets/img/veda-bot-bg.png";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-screen text-green-900" style={{
-      backgroundImage: `url(${bgimg})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center'
-    }}>
-      <div className="flex flex-col items-center">
-      <Image
-          className="h-36 w-36 mx-auto justify-center"
-          src={logo}
-          alt="Logo"
-        />
-        <Text className="text-center mb-10 text-green-900" fontWeight="bold" fontSize="4xl">
-          VedaBot
-        </Text>
+    <div
+      className="relative h-screen w-full text-green-900"
+      style={{
+        backgroundImage: `url(${bgimg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      {/* Spinner + Loading text centered */}
+      <div className="flex flex-col items-center justify-center h-full">
         <FaSpinner className="animate-spin text-green-900 text-6xl mb-3" />
         <Text className="text-center text-green-900" fontWeight="bold" fontSize="lg">
           Loading...
+        </Text>
+      </div>
+
+      {/* Bottom text */}
+      <div className="absolute bottom-10 w-full text-center px-4">
+        <Text
+          fontSize="5xl"
+          fontWeight="bold"
+          fontFamily="'Dancing Script', cursive"
+          color="green.900"
+        >
+          VedaBot
+        </Text>
+        <Text
+          fontSize="3xl"
+          fontWeight="bold"
+          fontFamily="'Dancing Script', cursive"
+          color="green.900"
+        >
+          Take your health into your hands
         </Text>
       </div>
     </div>
